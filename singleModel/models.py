00e6c17bfc75ebc94daad80e5b06a527094a62e9
sarorsaad <sarorsaad@gmail.com>
2022-09-02 21:09:55 +0300

    create model ,migration,migrate and register at admin

---
+++ singleModel/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=150)
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    movie_plot = models.TextField()
+    
+    def __str__(self):
+		   return self.movie_title
